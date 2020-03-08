82489951c93ffe4b3f61b6d14077ed1bfa8fc861
Ross Nomann <rossnomann@protonmail.com>
2020-03-08 21:40:19 +0400

    Introduce Command type

---
+++ src/types/command.rs
@@ -0,0 +1,147 @@
+use crate::types::message::Message;
+use shellwords::MismatchedQuotes;
+use std::{convert::TryFrom, error::Error, fmt, string::FromUtf16Error};
+
+/// A simple command implementation
+///
+/// We just take first command from a message and ignore others.
+/// Also we assume that all text after command is arguments separated by whitespace.
+/// In order to include space in argument you need to wrap it with `'`: `'arg1 v' arg2`.
+///
+/// # Example
+/// ```
+/// use tgbot::types::{Command, Message};
+/// use std::convert::TryFrom;
+///
+/// fn handle_command(message: Message) {
+///     let command = Command::try_from(message).unwrap();
+///     println!("NAME: {}", command.get_name());
+///     println!("ARGUMENTS: {:?}", command.get_args());
+///     println!("MESSAGE: {:?}", command.get_message());
+/// }
+/// ```
+#[derive(Clone, Debug)]
+pub struct Command {
+    name: String,
+    args: Vec<String>,
+    message: Message,
+}
+
+impl Command {
+    /// Returns command name with leading slash
+    pub fn get_name(&self) -> &str {
+        &self.name
+    }
+
+    /// Returns a list of arguments
+    pub fn get_args(&self) -> &[String] {
+        &self.args
+    }
+
+    /// Returns a message where command comes from
+    pub fn get_message(&self) -> &Message {
+        &self.message
+    }
+}
+
+/// An error when parsing command
+#[derive(Debug)]
+pub enum CommandError {
+    /// Command not found in a message
+    NotFound,
+    /// Failed to create an UTF-16 string when reading command from a message
+    Utf16(FromUtf16Error),
+    /// An error when splitting a string with mismatched quotes
+    MismatchedQuotes(MismatchedQuotes),
+}
+
+impl From<FromUtf16Error> for CommandError {
+    fn from(err: FromUtf16Error) -> Self {
+        Self::Utf16(err)
+    }
+}
+
+impl From<MismatchedQuotes> for CommandError {
+    fn from(err: MismatchedQuotes) -> Self {
+        Self::MismatchedQuotes(err)
+    }
+}
+
+impl Error for CommandError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            CommandError::NotFound => None,
+            CommandError::Utf16(err) => Some(err),
+            CommandError::MismatchedQuotes(_) => None,
+        }
+    }
+}
+
+impl fmt::Display for CommandError {
+    fn fmt(&self, out: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            out,
+            "failed to parse command: {}",
+            match self {
+                CommandError::NotFound => String::from("not found"),
+                CommandError::Utf16(err) => err.to_string(),
+                CommandError::MismatchedQuotes(_) => String::from("mismatched quotes"),
+            }
+        )
+    }
+}
+
+impl TryFrom<Message> for Command {
+    type Error = CommandError;
+
+    fn try_from(message: Message) -> Result<Self, Self::Error> {
+        match (&message.commands, message.get_text()) {
+            (Some(commands), Some(text)) => {
+                // tgbot guarantees that commands will never be empty, but we must be sure
+                assert!(!commands.is_empty());
+                // just take first command and ignore others
+                let command = &commands[0];
+                let name = command.command.clone();
+                // assume that all text after command is arguments
+                let pos = command.data.offset + command.data.length;
+                // pos is UTF-16 offset
+                let raw_args: Vec<u16> = text.data.encode_utf16().skip(pos).collect();
+                let raw_args = String::from_utf16(&raw_args)?;
+                let args = shellwords::split(&raw_args)?;
+                Ok(Command { name, args, message })
+            }
+            _ => Err(CommandError::NotFound),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn create_command(command: &str) -> Command {
+        let len = command.split_whitespace().next().unwrap().len();
+        let message: Message = serde_json::from_value(serde_json::json!(
+            {
+                "message_id": 1111,
+                "date": 0,
+                "from": {"id": 1, "is_bot": false, "first_name": "test"},
+                "chat": {"id": 1, "type": "private", "first_name": "test"},
+                "text": command,
+                "entities": [
+                    {"type": "bot_command", "offset": 0, "length": len}
+                ]
+            }
+        ))
+        .unwrap();
+        Command::try_from(message).unwrap()
+    }
+
+    #[test]
+    fn command() {
+        let command = create_command("/testcommand 'arg1 v' arg2");
+        assert_eq!(command.get_name(), "/testcommand");
+        assert_eq!(command.get_args(), &["arg1 v", "arg2"]);
+        assert_eq!(command.get_message().id, 1111);
+    }
+}
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -2,6 +2,7 @@ mod animation;
 mod audio;
 mod callback_query;
 mod chat;
+mod command;
 mod contact;
 mod document;
 mod file;
@@ -29,8 +30,8 @@ mod video_note;
 mod voice;
 
 pub use self::{
-    animation::*, audio::*, callback_query::*, chat::*, contact::*, document::*, file::*, game::*, inline_mode::*,
-    input_media::*, location::*, login_url::*, media_group::*, message::*, parse_mode::*, passport::*, payments::*,
-    photo_size::*, poll::*, primitive::*, reply_markup::*, response::*, stickers::*, update::*, user::*, venue::*,
-    video::*, video_note::*, voice::*,
+    animation::*, audio::*, callback_query::*, chat::*, command::*, contact::*, document::*, file::*, game::*,
+    inline_mode::*, input_media::*, location::*, login_url::*, media_group::*, message::*, parse_mode::*, passport::*,
+    payments::*, photo_size::*, poll::*, primitive::*, reply_markup::*, response::*, stickers::*, update::*, user::*,
+    venue::*, video::*, video_note::*, voice::*,
 };

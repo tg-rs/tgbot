1679aebad3e869b1e0dc2f81d07e4bf2ac152160
Ross Nomann <rossnomann@protonmail.com>
2020-11-14 00:13:30 +0300

    [Bot API 5.0] Update docs for PinChatMessage method

--- src/methods/chat/pin_message.rs
+++ src/methods/chat/pin_message.rs
@@ -5,11 +5,11 @@ use crate::{
 };
 use serde::Serialize;
 
-/// Pin a message in a group, supergroup or a channel
+/// Add a message to the list of pinned messages in a chat
 ///
-/// The bot must be an administrator in the chat for this
-/// to work and must have the ‘can_pin_messages’ admin right
-/// in the supergroup or ‘can_edit_messages’ admin right in the channel
+/// If the chat is not a private chat, the bot must be an administrator
+/// in the chat for this to work and must have the 'can_pin_messages'
+/// admin right in a supergroup or 'can_edit_messages' admin right in a channel.
 #[derive(Clone, Debug, Serialize)]
 pub struct PinChatMessage {
     chat_id: ChatId,

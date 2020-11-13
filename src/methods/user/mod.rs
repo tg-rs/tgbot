e0f049c8063dfb4e9eed417d43c3eca1fd1409b0
Ross Nomann <rossnomann@protonmail.com>
2020-11-13 21:57:25 +0300

    [Bot API 5.0] Add close method

--- src/methods/user/mod.rs
+++ src/methods/user/mod.rs
@@ -1,5 +1,6 @@
+mod close;
 mod get_me;
 mod get_profile_photos;
 mod log_out;
 
-pub use self::{get_me::*, get_profile_photos::*, log_out::*};
+pub use self::{close::*, get_me::*, get_profile_photos::*, log_out::*};

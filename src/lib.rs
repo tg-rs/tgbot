6e94465ce693be0905e5901277fa5351ef3f9d54
ark0f <arsenylyashenko.3@gmail.com>
2021-02-19 21:40:46 +0300

    Add SyncedUpdateHandler

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@
 #![warn(missing_docs)]
 
 mod api;
-mod handler;
 mod request;
 
 /// Utilities to receive updates using long poll
@@ -19,6 +18,9 @@ pub mod types;
 
 /// Services to receive updates via webhook
 pub mod webhook;
+
+/// UpdateHandler trait and utilities for it
+pub mod handler;
 
 pub use self::{
     api::{Api, ApiError, Config, DownloadFileError, ExecuteError, ParseProxyError},

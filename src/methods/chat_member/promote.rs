3d99ee90ffb9a2361e4141cc7c0fab3a88298466
Ross Nomann <rossnomann@protonmail.com>
2021-03-14 08:51:53 +0100

    Improve tests for PromoteChatMember method

--- src/methods/chat_member/promote.rs
+++ src/methods/chat_member/promote.rs
@@ -19,23 +19,23 @@ pub struct PromoteChatMember {
     #[serde(skip_serializing_if = "Option::is_none")]
     can_change_info: Option<bool>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    can_post_messages: Option<bool>,
+    can_delete_messages: Option<bool>,
     #[serde(skip_serializing_if = "Option::is_none")]
     can_edit_messages: Option<bool>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    can_delete_messages: Option<bool>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     can_invite_users: Option<bool>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    can_restrict_members: Option<bool>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    can_pin_messages: Option<bool>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    can_promote_members: Option<bool>,
+    can_manage_chat: Option<bool>,
     #[serde(skip_serializing_if = "Option::is_none")]
     can_manage_voice_chats: Option<bool>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    can_manage_chat: Option<bool>,
+    can_pin_messages: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    can_post_messages: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    can_promote_members: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    can_restrict_members: Option<bool>,
 }
 
 impl PromoteChatMember {
@@ -51,15 +51,15 @@ impl PromoteChatMember {
             user_id,
             is_anonymous: None,
             can_change_info: None,
-            can_post_messages: None,
-            can_edit_messages: None,
             can_delete_messages: None,
+            can_edit_messages: None,
             can_invite_users: None,
-            can_restrict_members: None,
-            can_pin_messages: None,
-            can_promote_members: None,
-            can_manage_voice_chats: None,
             can_manage_chat: None,
+            can_manage_voice_chats: None,
+            can_pin_messages: None,
+            can_post_messages: None,
+            can_promote_members: None,
+            can_restrict_members: None,
         }
     }
 
@@ -67,15 +67,15 @@ impl PromoteChatMember {
     pub fn promote_all(mut self) -> Self {
         self.is_anonymous = Some(true);
         self.can_change_info = Some(true);
-        self.can_post_messages = Some(true);
-        self.can_edit_messages = Some(true);
         self.can_delete_messages = Some(true);
+        self.can_edit_messages = Some(true);
         self.can_invite_users = Some(true);
-        self.can_restrict_members = Some(true);
-        self.can_pin_messages = Some(true);
-        self.can_promote_members = Some(true);
-        self.can_manage_voice_chats = Some(true);
         self.can_manage_chat = Some(true);
+        self.can_manage_voice_chats = Some(true);
+        self.can_pin_messages = Some(true);
+        self.can_post_messages = Some(true);
+        self.can_promote_members = Some(true);
+        self.can_restrict_members = Some(true);
         self
     }
 
@@ -83,15 +83,15 @@ impl PromoteChatMember {
     pub fn demote_all(mut self) -> Self {
         self.is_anonymous = Some(false);
         self.can_change_info = Some(false);
-        self.can_post_messages = Some(false);
-        self.can_edit_messages = Some(false);
         self.can_delete_messages = Some(false);
+        self.can_edit_messages = Some(false);
         self.can_invite_users = Some(false);
-        self.can_restrict_members = Some(false);
-        self.can_pin_messages = Some(false);
-        self.can_promote_members = Some(false);
-        self.can_manage_voice_chats = Some(false);
         self.can_manage_chat = Some(false);
+        self.can_manage_voice_chats = Some(false);
+        self.can_pin_messages = Some(false);
+        self.can_post_messages = Some(false);
+        self.can_promote_members = Some(false);
+        self.can_restrict_members = Some(false);
         self
     }
 
@@ -108,9 +108,9 @@ impl PromoteChatMember {
         self
     }
 
-    /// Administrator can create channel posts, channels only
-    pub fn can_post_messages(mut self, can_post_messages: bool) -> Self {
-        self.can_post_messages = Some(can_post_messages);
+    /// Administrator can delete messages of other users
+    pub fn can_delete_messages(mut self, can_delete_messages: bool) -> Self {
+        self.can_delete_messages = Some(can_delete_messages);
         self
     }
 
@@ -120,40 +120,9 @@ impl PromoteChatMember {
         self
     }
 
-    /// Administrator can delete messages of other users
-    pub fn can_delete_messages(mut self, can_delete_messages: bool) -> Self {
-        self.can_delete_messages = Some(can_delete_messages);
-        self
-    }
-
     /// Administrator can invite new users to the chat
     pub fn can_invite_users(mut self, can_invite_users: bool) -> Self {
         self.can_invite_users = Some(can_invite_users);
-        self
-    }
-
-    /// Administrator can restrict, ban or unban chat members
-    pub fn can_restrict_members(mut self, can_restrict_members: bool) -> Self {
-        self.can_restrict_members = Some(can_restrict_members);
-        self
-    }
-
-    /// Administrator can pin messages, supergroups only
-    pub fn can_pin_messages(mut self, can_pin_messages: bool) -> Self {
-        self.can_pin_messages = Some(can_pin_messages);
-        self
-    }
-
-    /// Administrator can add new administrators with a subset of his own privileges or demote administrators
-    /// that he has promoted, directly or indirectly (promoted by administrators that were appointed by him)
-    pub fn can_promote_members(mut self, can_promote_members: bool) -> Self {
-        self.can_promote_members = Some(can_promote_members);
-        self
-    }
-
-    /// Administrator can manage voice chats, supergroups only
-    pub fn can_manage_voice_chats(mut self, can_manage_voice_chats: bool) -> Self {
-        self.can_manage_voice_chats = Some(can_manage_voice_chats);
         self
     }
 
@@ -164,6 +133,37 @@ impl PromoteChatMember {
     /// Implied by any other administrator privilege
     pub fn can_manage_chat(mut self, can_manage_chat: bool) -> Self {
         self.can_manage_chat = Some(can_manage_chat);
+        self
+    }
+
+    /// Administrator can manage voice chats, supergroups only
+    pub fn can_manage_voice_chats(mut self, can_manage_voice_chats: bool) -> Self {
+        self.can_manage_voice_chats = Some(can_manage_voice_chats);
+        self
+    }
+
+    /// Administrator can pin messages, supergroups only
+    pub fn can_pin_messages(mut self, can_pin_messages: bool) -> Self {
+        self.can_pin_messages = Some(can_pin_messages);
+        self
+    }
+
+    /// Administrator can create channel posts, channels only
+    pub fn can_post_messages(mut self, can_post_messages: bool) -> Self {
+        self.can_post_messages = Some(can_post_messages);
+        self
+    }
+
+    /// Administrator can add new administrators with a subset of his own privileges or demote administrators
+    /// that he has promoted, directly or indirectly (promoted by administrators that were appointed by him)
+    pub fn can_promote_members(mut self, can_promote_members: bool) -> Self {
+        self.can_promote_members = Some(can_promote_members);
+        self
+    }
+
+    /// Administrator can restrict, ban or unban chat members
+    pub fn can_restrict_members(mut self, can_restrict_members: bool) -> Self {
+        self.can_restrict_members = Some(can_restrict_members);
         self
     }
 }
@@ -192,19 +192,24 @@ mod tests {
         );
         if let RequestBody::Json(data) = request.into_body() {
             let data: Value = serde_json::from_str(&data.unwrap()).unwrap();
-            assert_eq!(data["chat_id"], 1);
-            assert_eq!(data["user_id"], 2);
-            assert_eq!(data["is_anonymous"], true);
-            assert_eq!(data["can_change_info"], true);
-            assert_eq!(data["can_post_messages"], true);
-            assert_eq!(data["can_edit_messages"], true);
-            assert_eq!(data["can_delete_messages"], true);
-            assert_eq!(data["can_invite_users"], true);
-            assert_eq!(data["can_restrict_members"], true);
-            assert_eq!(data["can_pin_messages"], true);
-            assert_eq!(data["can_promote_members"], true);
-            assert_eq!(data["can_manage_voice_chats"], true);
-            assert_eq!(data["can_manage_chat"], true);
+            assert_eq!(
+                data,
+                serde_json::json!({
+                    "chat_id": 1,
+                    "user_id": 2,
+                    "is_anonymous": true,
+                    "can_change_info": true,
+                    "can_delete_messages": true,
+                    "can_edit_messages": true,
+                    "can_invite_users": true,
+                    "can_manage_chat": true,
+                    "can_manage_voice_chats": true,
+                    "can_pin_messages": true,
+                    "can_post_messages": true,
+                    "can_promote_members": true,
+                    "can_restrict_members": true
+                })
+            );
         } else {
             panic!("Unexpected request body");
         }
@@ -220,19 +225,24 @@ mod tests {
         );
         if let RequestBody::Json(data) = request.into_body() {
             let data: Value = serde_json::from_str(&data.unwrap()).unwrap();
-            assert_eq!(data["chat_id"], 1);
-            assert_eq!(data["user_id"], 2);
-            assert_eq!(data["is_anonymous"], false);
-            assert_eq!(data["can_change_info"], false);
-            assert_eq!(data["can_post_messages"], false);
-            assert_eq!(data["can_edit_messages"], false);
-            assert_eq!(data["can_delete_messages"], false);
-            assert_eq!(data["can_invite_users"], false);
-            assert_eq!(data["can_restrict_members"], false);
-            assert_eq!(data["can_pin_messages"], false);
-            assert_eq!(data["can_promote_members"], false);
-            assert_eq!(data["can_manage_voice_chats"], false);
-            assert_eq!(data["can_manage_chat"], false);
+            assert_eq!(
+                data,
+                serde_json::json!({
+                    "chat_id": 1,
+                    "user_id": 2,
+                    "is_anonymous": false,
+                    "can_change_info": false,
+                    "can_delete_messages": false,
+                    "can_edit_messages": false,
+                    "can_invite_users": false,
+                    "can_manage_chat": false,
+                    "can_manage_voice_chats": false,
+                    "can_pin_messages": false,
+                    "can_post_messages": false,
+                    "can_promote_members": false,
+                    "can_restrict_members": false
+                })
+            );
         } else {
             panic!("Unexpected request body");
         }
@@ -243,15 +253,15 @@ mod tests {
         let request = PromoteChatMember::new(1, 2)
             .is_anonymous(false)
             .can_change_info(true)
-            .can_post_messages(false)
             .can_edit_messages(true)
             .can_delete_messages(false)
             .can_invite_users(true)
-            .can_restrict_members(false)
-            .can_pin_messages(true)
-            .can_promote_members(false)
-            .can_manage_voice_chats(true)
             .can_manage_chat(false)
+            .can_manage_voice_chats(true)
+            .can_pin_messages(true)
+            .can_post_messages(false)
+            .can_promote_members(false)
+            .can_restrict_members(false)
             .into_request();
         assert_eq!(request.get_method(), RequestMethod::Post);
         assert_eq!(
@@ -260,19 +270,46 @@ mod tests {
         );
         if let RequestBody::Json(data) = request.into_body() {
             let data: Value = serde_json::from_str(&data.unwrap()).unwrap();
-            assert_eq!(data["chat_id"], 1);
-            assert_eq!(data["user_id"], 2);
-            assert_eq!(data["is_anonymous"], false);
-            assert_eq!(data["can_change_info"], true);
-            assert_eq!(data["can_post_messages"], false);
-            assert_eq!(data["can_edit_messages"], true);
-            assert_eq!(data["can_delete_messages"], false);
-            assert_eq!(data["can_invite_users"], true);
-            assert_eq!(data["can_restrict_members"], false);
-            assert_eq!(data["can_pin_messages"], true);
-            assert_eq!(data["can_promote_members"], false);
-            assert_eq!(data["can_manage_voice_chats"], true);
-            assert_eq!(data["can_manage_chat"], false);
+            assert_eq!(
+                data,
+                serde_json::json!({
+                    "chat_id": 1,
+                    "user_id": 2,
+                    "is_anonymous": false,
+                    "can_change_info": true,
+                    "can_delete_messages": false,
+                    "can_edit_messages": true,
+                    "can_invite_users": true,
+                    "can_manage_chat": false,
+                    "can_manage_voice_chats": true,
+                    "can_pin_messages": true,
+                    "can_post_messages": false,
+                    "can_promote_members": false,
+                    "can_restrict_members": false
+                })
+            );
+        } else {
+            panic!("Unexpected request body");
+        }
+    }
+
+    #[test]
+    fn promote_chat_member_empty() {
+        let request = PromoteChatMember::new(1, 2).into_request();
+        assert_eq!(request.get_method(), RequestMethod::Post);
+        assert_eq!(
+            request.build_url("base-url", "token"),
+            "base-url/bottoken/promoteChatMember"
+        );
+        if let RequestBody::Json(data) = request.into_body() {
+            let data: Value = serde_json::from_str(&data.unwrap()).unwrap();
+            assert_eq!(
+                data,
+                serde_json::json!({
+                    "chat_id": 1,
+                    "user_id": 2
+                })
+            );
         } else {
             panic!("Unexpected request body");
         }

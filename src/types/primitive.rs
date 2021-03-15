558eff2cde406ff188c23b94323c3c59a9aba3e9
ark0f <arsenylyashenko.3@gmail.com>
2021-03-15 18:48:04 +0300

    Implement Serialize for True and False

--- src/types/primitive.rs
+++ src/types/primitive.rs
@@ -1,6 +1,6 @@
 use serde::{
     de::{Error, Unexpected},
-    Deserialize, Deserializer,
+    Deserialize, Deserializer, Serialize, Serializer,
 };
 use std::fmt;
 
@@ -21,6 +21,15 @@ macro_rules! impl_bool_type {
     ($name:ident = $value:ident) => {
         #[derive(Clone, Copy)]
         pub(crate) struct $name;
+
+        impl Serialize for $name {
+            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+            where
+                S: Serializer,
+            {
+                serializer.serialize_bool($value)
+            }
+        }
 
         impl<'de> Deserialize<'de> for $name {
             fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
@@ -51,7 +60,17 @@ impl_bool_type!(False = false);
 mod tests {
     use super::*;
 
-    macro_rules! test_bool {
+    macro_rules! test_bool_ser {
+        ($fn:ident($name:ident = $value:ident)) => {
+            #[test]
+            fn $fn() {
+                let value = serde_json::to_string(&$name).unwrap();
+                assert_eq!(value, stringify!($value));
+            }
+        };
+    }
+
+    macro_rules! test_bool_de {
         ($( #[should_panic = $msg:literal] )? $fn:ident($name:ident = $value:ident)) => {
             #[test]
             $( #[should_panic = $msg] )?
@@ -61,13 +80,16 @@ mod tests {
         };
     }
 
-    test_bool!(deserialize_true(True = true));
-    test_bool! {
+    test_bool_ser!(serialize_true(True = true));
+    test_bool_ser!(serialize_false(False = false));
+
+    test_bool_de!(deserialize_true(True = true));
+    test_bool_de! {
         #[should_panic = "invalid value: boolean `false`, expected true"]
         deserialize_true_unexpected(True = false)
     }
-    test_bool!(deserialize_false(False = false));
-    test_bool! {
+    test_bool_de!(deserialize_false(False = false));
+    test_bool_de! {
         #[should_panic = "invalid value: boolean `true`, expected false"]
         deserialize_false_unexpected(False = true)
     }

79dd0320278bf685edc447e386b488ec3a3c04c1
Ross Nomann <rossnomann@protonmail.com>
2021-09-05 20:37:36 +0200

    [Bot API 5.2] Add InputMessageContentInvoice struct

--- src/types/inline_mode/message_content.rs
+++ src/types/inline_mode/message_content.rs
@@ -1,9 +1,11 @@
 use crate::types::{
     parse_mode::ParseMode,
+    payments::LabeledPrice,
     primitive::{Float, Integer},
     text::TextEntity,
 };
 use serde::Serialize;
+use serde_json::Error as JsonError;
 
 /// Content of a message to be sent as a result of an inline query
 #[derive(Clone, Debug, derive_more::From, Serialize)]
@@ -11,6 +13,8 @@ use serde::Serialize;
 pub enum InputMessageContent {
     /// Contact message
     Contact(InputMessageContentContact),
+    /// Invoice message
+    Invoice(InputMessageContentInvoice),
     /// Location message
     Location(InputMessageContentLocation),
     /// Text message
@@ -55,6 +59,206 @@ impl InputMessageContentContact {
     /// Additional data about the contact in the form of a vCard, 0-2048 bytes
     pub fn vcard<S: Into<String>>(mut self, vcard: S) -> Self {
         self.vcard = Some(vcard.into());
+        self
+    }
+}
+
+/// Represents the content of an invoice message to be sent as the result of an inline query
+#[derive(Clone, Debug, Serialize)]
+pub struct InputMessageContentInvoice {
+    title: String,
+    description: String,
+    payload: String,
+    provider_token: String,
+    currency: String,
+    prices: Vec<LabeledPrice>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    max_tip_amount: Option<Integer>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    suggested_tip_amounts: Option<Vec<Integer>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    provider_data: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    photo_url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    photo_size: Option<Integer>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    photo_width: Option<Integer>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    photo_height: Option<Integer>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    need_name: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    need_phone_number: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    need_email: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    need_shipping_address: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    send_phone_number_to_provider: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    send_email_to_provider: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    is_flexible: Option<bool>,
+}
+
+impl InputMessageContentInvoice {
+    /// Creates a new InputMessageContentInvoice
+    ///
+    /// # Arguments
+    ///
+    /// * title - Product name, 1-32 characters
+    /// * description - Product description, 1-255 characters
+    /// * payload - Bot-defined invoice payload, 1-128 bytes.
+    ///             This will not be displayed to the user,
+    ///             use for your internal processes.
+    /// * provider_token - Payment provider token, obtained via Botfather
+    /// * currency - Three-letter ISO 4217 currency code, see more on currencies
+    /// * prices - Price breakdown  (e.g. product price, tax, discount,
+    ///            delivery cost, delivery tax, bonus, etc.)
+    pub fn new<A, B, C, D, E, F>(
+        title: A,
+        description: B,
+        payload: C,
+        provider_token: D,
+        currency: E,
+        prices: F,
+    ) -> Self
+    where
+        A: Into<String>,
+        B: Into<String>,
+        C: Into<String>,
+        D: Into<String>,
+        E: Into<String>,
+        F: IntoIterator<Item = LabeledPrice>,
+    {
+        Self {
+            title: title.into(),
+            description: description.into(),
+            payload: payload.into(),
+            provider_token: provider_token.into(),
+            currency: currency.into(),
+            prices: prices.into_iter().collect(),
+            max_tip_amount: None,
+            suggested_tip_amounts: None,
+            provider_data: None,
+            photo_url: None,
+            photo_size: None,
+            photo_width: None,
+            photo_height: None,
+            need_name: None,
+            need_phone_number: None,
+            need_email: None,
+            need_shipping_address: None,
+            send_phone_number_to_provider: None,
+            send_email_to_provider: None,
+            is_flexible: None,
+        }
+    }
+
+    /// Sets the maximum accepted amount for tips in the smallest units of the currency
+    ///
+    /// For example, for a maximum tip of US$ 1.45 pass max_tip_amount = 145.
+    /// See the exp parameter in [currencies.json], it shows the number
+    // of digits past the decimal point for each currency
+    /// (2 for the majority of currencies). Defaults to 0
+    ///
+    /// [currencies.json]: https://core.telegram.org/bots/payments/currencies.json
+    pub fn max_tip_amount(mut self, value: Integer) -> Self {
+        self.max_tip_amount = Some(value);
+        self
+    }
+
+    /// Sets an array of suggested amounts of tip in the smallest units of the currency
+    ///
+    /// At most 4 suggested tip amounts can be specified.
+    /// The suggested tip amounts must be positive, passed
+    /// in a strictly increased order and must not exceed max_tip_amount.
+    pub fn suggested_tip_amounts(mut self, value: impl IntoIterator<Item = Integer>) -> Self {
+        self.suggested_tip_amounts = Some(value.into_iter().collect());
+        self
+    }
+
+    /// An object for data about the invoice,
+    /// which will be shared with the payment provider.
+    /// A detailed description of the required fields should be provided by the payment provider.
+    pub fn provider_data<T>(mut self, value: &T) -> Result<Self, JsonError>
+    where
+        T: Serialize,
+    {
+        self.provider_data = Some(serde_json::to_string(value)?);
+        Ok(self)
+    }
+
+    /// Sets an URL of the product photo for the invoice
+    ///
+    /// Can be a photo of the goods or a marketing image for a service.
+    /// People like it better when they see what they are paying for.
+    pub fn photo_url<T>(mut self, value: T) -> Self
+    where
+        T: Into<String>,
+    {
+        self.photo_url = Some(value.into());
+        self
+    }
+
+    /// Sets a photo size
+    pub fn photo_size(mut self, value: Integer) -> Self {
+        self.photo_size = Some(value);
+        self
+    }
+
+    /// Sets a photo width
+    pub fn photo_width(mut self, value: Integer) -> Self {
+        self.photo_width = Some(value);
+        self
+    }
+
+    /// Sets a photo height
+    pub fn photo_height(mut self, value: Integer) -> Self {
+        self.photo_height = Some(value);
+        self
+    }
+
+    /// Pass True, if you require the user's full name to complete the order
+    pub fn need_name(mut self, value: bool) -> Self {
+        self.need_name = Some(value);
+        self
+    }
+
+    /// Pass True, if you require the user's phone number to complete the order
+    pub fn need_phone_number(mut self, value: bool) -> Self {
+        self.need_phone_number = Some(value);
+        self
+    }
+
+    /// Pass True, if you require the user's email address to complete the order
+    pub fn need_email(mut self, value: bool) -> Self {
+        self.need_email = Some(value);
+        self
+    }
+
+    /// Pass True, if you require the user's shipping address to complete the order
+    pub fn need_shipping_address(mut self, value: bool) -> Self {
+        self.need_shipping_address = Some(value);
+        self
+    }
+
+    /// Pass True, if user's phone number should be sent to provider
+    pub fn send_phone_number_to_provider(mut self, value: bool) -> Self {
+        self.send_phone_number_to_provider = Some(value);
+        self
+    }
+
+    /// Pass True, if user's email address should be sent to provider
+    pub fn send_email_to_provider(mut self, value: bool) -> Self {
+        self.send_email_to_provider = Some(value);
+        self
+    }
+
+    /// Pass True, if the final price depends on the shipping method
+    pub fn is_flexible(mut self, value: bool) -> Self {
+        self.is_flexible = Some(value);
         self
     }
 }
@@ -275,6 +479,93 @@ mod tests {
                 "first_name": "Vasya"
             })
         );
+    }
+
+    #[derive(Serialize)]
+    struct InvoiceProviderData {
+        key: String,
+    }
+
+    #[test]
+    fn serialize_invoice() {
+        let val = serde_json::to_value(InputMessageContent::from(
+            InputMessageContentInvoice::new(
+                "title",
+                "description",
+                "payload",
+                "provider-token",
+                "RUB",
+                [LabeledPrice::new("item", 100)],
+            )
+            .max_tip_amount(1)
+            .suggested_tip_amounts([2])
+            .provider_data(&InvoiceProviderData {
+                key: String::from("value"),
+            })
+            .unwrap()
+            .photo_url("https://google.com/favicon.ico")
+            .photo_size(100)
+            .photo_width(24)
+            .photo_height(24)
+            .need_name(true)
+            .need_email(false)
+            .need_phone_number(true)
+            .need_shipping_address(false)
+            .send_phone_number_to_provider(true)
+            .send_email_to_provider(false)
+            .is_flexible(true),
+        ))
+        .unwrap();
+        assert_eq!(val["title"], "title");
+        assert_eq!(val["description"], "description");
+        assert_eq!(val["payload"], "payload");
+        assert_eq!(val["provider_token"], "provider-token");
+        assert_eq!(val["currency"], "RUB");
+        assert_eq!(val["prices"], serde_json::json!([{"label": "item", "amount": 100}]));
+        assert_eq!(val["max_tip_amount"], 1);
+        assert_eq!(val["suggested_tip_amounts"].as_array().unwrap(), &[2]);
+        assert_eq!(val["provider_data"], r#"{"key":"value"}"#);
+        assert_eq!(val["photo_url"], "https://google.com/favicon.ico");
+        assert_eq!(val["photo_size"], 100);
+        assert_eq!(val["photo_width"], 24);
+        assert_eq!(val["photo_height"], 24);
+        assert!(val["need_name"].as_bool().unwrap());
+        assert!(!val["need_email"].as_bool().unwrap());
+        assert!(val["need_phone_number"].as_bool().unwrap());
+        assert!(!val["need_shipping_address"].as_bool().unwrap());
+        assert!(val["send_phone_number_to_provider"].as_bool().unwrap());
+        assert!(!val["send_email_to_provider"].as_bool().unwrap());
+        assert!(val["is_flexible"].as_bool().unwrap());
+
+        let val = serde_json::to_value(InputMessageContent::from(InputMessageContentInvoice::new(
+            "title",
+            "description",
+            "payload",
+            "provider-token",
+            "RUB",
+            [LabeledPrice::new("item", 100)],
+        )))
+        .unwrap();
+        assert_eq!(val["title"], "title");
+        assert_eq!(val["description"], "description");
+        assert_eq!(val["payload"], "payload");
+        assert_eq!(val["provider_token"], "provider-token");
+        assert_eq!(val["currency"], "RUB");
+        assert_eq!(val["prices"], serde_json::json!([{"label": "item", "amount": 100}]));
+        assert!(val.get("max_tip_amount").is_none());
+        assert!(val.get("suggested_tip_amounts").is_none());
+        assert!(val.get("provider_data").is_none());
+        assert!(val.get("photo_url").is_none());
+        assert!(val.get("photo_size").is_none());
+        assert!(val.get("photo_width").is_none());
+        assert!(val.get("photo_height").is_none());
+        assert!(val.get("need_name").is_none());
+        assert!(val.get("need_phone_number").is_none());
+        assert!(val.get("need_email").is_none());
+        assert!(val.get("need_shipping_address").is_none());
+        assert!(val.get("send_phone_number_to_provider").is_none());
+        assert!(val.get("send_email_to_provider").is_none());
+        assert!(val.get("is_flexible").is_none());
     }
 
     #[allow(clippy::float_cmp)]

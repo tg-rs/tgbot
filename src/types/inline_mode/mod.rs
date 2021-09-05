6c2ae523115d98bbb8f226947b95add66e17f273
Ross Nomann <rossnomann@protonmail.com>
2021-09-05 21:19:08 +0200

    [Bot API 5.2] Add InlineQuery.chat_type field

--- src/types/inline_mode/mod.rs
+++ src/types/inline_mode/mod.rs
@@ -15,12 +15,36 @@ pub struct InlineQuery {
     pub id: String,
     /// Sender
     pub from: User,
-    /// Sender location, only for bots that request user location
-    pub location: Option<Location>,
     /// Text of the query (up to 512 characters)
     pub query: String,
     /// Offset of the results to be returned, can be controlled by the bot
     pub offset: String,
+    /// Type of the chat, from which the inline query was sent
+    ///
+    /// Can be either “sender” for a private chat with the inline query sender,
+    /// “private”, “group”, “supergroup”, or “channel”.
+    /// The chat type should be always known for requests sent from official
+    /// clients and most third-party clients,
+    /// unless the request was sent from a secret chat
+    pub chat_type: Option<InlineQueryChatType>,
+    /// Sender location, only for bots that request user location
+    pub location: Option<Location>,
+}
+
+/// Type of the chat, from which the inline query was sent
+#[derive(Clone, Copy, Debug, Deserialize, PartialEq, PartialOrd)]
+#[serde(rename_all = "snake_case")]
+pub enum InlineQueryChatType {
+    /// Private chat with the inline query sender
+    Sender,
+    /// Private chat
+    Private,
+    /// Group
+    Group,
+    /// Supergroup
+    Supergroup,
+    /// Channel
+    Channel,
 }
 
 /// Result of an inline query that was chosen by the user and sent to their chat partner
@@ -60,6 +84,7 @@ mod tests {
             },
             "query": "query string",
             "offset": "query offset",
+            "chat_type": "private"
         }))
         .unwrap();
         assert_eq!(data.id, "query id");
@@ -67,6 +92,7 @@ mod tests {
         assert_eq!(data.location.unwrap().latitude, 2.1);
         assert_eq!(data.query, "query string");
         assert_eq!(data.offset, "query offset");
+        assert_eq!(data.chat_type.unwrap(), InlineQueryChatType::Private);
     }
 
     #[allow(clippy::float_cmp)]

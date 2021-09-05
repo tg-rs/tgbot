aab1387adb78fecf2a7198737ec7b73a68ee781a
Ross Nomann <rossnomann@protonmail.com>
2021-09-06 00:07:36 +0200

    Include readme on stable

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 //! A Telegram Bot API client library
-#![cfg_attr(nightly, feature(doc_cfg, external_doc))]
-#![cfg_attr(nightly, doc(include = "../README.md"))]
+#![doc = include_str!("../README.md")]
 #![recursion_limit = "256"]
 #![warn(missing_docs)]
 

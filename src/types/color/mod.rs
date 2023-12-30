4b9fcb5c59a5092b4ad6aafed5b945c59869e210
Ross Nomann <rossnomann@protonmail.com>
2023-12-30 13:34:05 +0100

    [Bot API 7.0] Add support for accent colors
    - Added types: AccentColor, ProfileAccentColor.

--- src/types/color/mod.rs
+++ src/types/color/mod.rs
@@ -1,8 +1,87 @@
 use crate::types::Integer;
 use serde::{Deserialize, Serialize};
+use std::{error::Error, fmt};
 
 #[cfg(test)]
 mod tests;
+
+/// Represents an accent color.
+///
+/// See the [documentation][1] for more information.
+///
+/// [1]: https://core.telegram.org/bots/api#accent-colors
+#[derive(Clone, Copy, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Hash, Serialize)]
+#[serde(into = "Integer", try_from = "Integer")]
+pub enum AccentColor {
+    /// Color ID: 5.
+    Blue,
+    /// Color ID: 4.
+    Cyan,
+    /// Color ID: 3.
+    Green,
+    /// Color ID: 1.
+    Orange,
+    /// Color ID: 6.
+    Pink,
+    /// Color ID: 2.
+    Purple,
+    /// Color ID: 0.
+    Red,
+    /// Color ID: 7-20.
+    Unknown(Integer),
+}
+
+impl TryFrom<Integer> for AccentColor {
+    type Error = AccentColorError;
+
+    fn try_from(value: Integer) -> Result<Self, Self::Error> {
+        use self::AccentColor::*;
+        Ok(match value {
+            0 => Red,
+            1 => Orange,
+            2 => Purple,
+            3 => Green,
+            4 => Cyan,
+            5 => Blue,
+            6 => Pink,
+            7..=20 => Unknown(value),
+            _ => return Err(AccentColorError::UnknownId(value)),
+        })
+    }
+}
+
+impl From<AccentColor> for Integer {
+    fn from(value: AccentColor) -> Self {
+        use self::AccentColor::*;
+        match value {
+            Red => 0,
+            Orange => 1,
+            Purple => 2,
+            Green => 3,
+            Cyan => 4,
+            Blue => 5,
+            Pink => 6,
+            Unknown(value) => value,
+        }
+    }
+}
+
+/// Represents an accent color error.
+#[derive(Debug)]
+pub enum AccentColorError {
+    /// Got an unknown color ID.
+    UnknownId(Integer),
+}
+
+impl Error for AccentColorError {}
+
+impl fmt::Display for AccentColorError {
+    fn fmt(&self, out: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            AccentColorError::UnknownId(value) => write!(out, "unknown accent color ID: {}", value),
+        }
+    }
+}
 
 /// Represents a color of a forum topic icon.
 #[derive(Clone, Copy, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Hash, Serialize)]
@@ -50,6 +129,50 @@ impl From<ForumTopicIconColor> for Integer {
             LightGreen => 9367192,
             VeryLightAzure => 7322096,
             Unknown(value) => value,
+        }
+    }
+}
+
+/// Represents a profile accent color.
+///
+/// See the [documentation][1] for more information.
+///
+/// [1]: https://core.telegram.org/bots/api#profile-accent-colors
+#[derive(Clone, Copy, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Hash, Serialize)]
+#[serde(into = "Integer", try_from = "Integer")]
+pub struct ProfileAccentColor(Integer);
+
+impl TryFrom<Integer> for ProfileAccentColor {
+    type Error = ProfileAccentColorError;
+
+    fn try_from(value: Integer) -> Result<Self, Self::Error> {
+        if !(0..=15).contains(&value) {
+            Err(ProfileAccentColorError::UnknownId(value))
+        } else {
+            Ok(Self(value))
+        }
+    }
+}
+
+impl From<ProfileAccentColor> for Integer {
+    fn from(value: ProfileAccentColor) -> Self {
+        value.0
+    }
+}
+
+/// Represents a profile accent color error.
+#[derive(Debug)]
+pub enum ProfileAccentColorError {
+    /// Got an unknown color ID.
+    UnknownId(Integer),
+}
+
+impl Error for ProfileAccentColorError {}
+
+impl fmt::Display for ProfileAccentColorError {
+    fn fmt(&self, out: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ProfileAccentColorError::UnknownId(value) => write!(out, "unknown profile accent color ID: {}", value),
         }
     }
 }
--- src/types/color/tests.rs
+++ src/types/color/tests.rs
@@ -1,4 +1,29 @@
-use crate::types::{tests::assert_json_eq, ForumTopicIconColor, Integer};
+use crate::types::{tests::assert_json_eq, AccentColor, ForumTopicIconColor, Integer, ProfileAccentColor};
+
+#[test]
+fn accent_color() {
+    for (expected_struct, expected_value) in [
+        (AccentColor::Red, 0),
+        (AccentColor::Orange, 1),
+        (AccentColor::Purple, 2),
+        (AccentColor::Green, 3),
+        (AccentColor::Cyan, 4),
+        (AccentColor::Blue, 5),
+        (AccentColor::Pink, 6),
+    ] {
+        assert_eq!(Integer::from(expected_struct), expected_value);
+        assert_eq!(AccentColor::try_from(expected_value).unwrap(), expected_struct);
+        assert_json_eq(expected_struct, serde_json::json!(expected_value))
+    }
+    for i in 7..=20 {
+        let expected_struct = AccentColor::try_from(i).unwrap();
+        assert_eq!(Integer::from(expected_struct), i);
+        assert_json_eq(expected_struct, serde_json::json!(i));
+    }
+    assert!(AccentColor::try_from(-1).is_err());
+    assert!(AccentColor::try_from(20).is_ok());
+    assert!(AccentColor::try_from(21).is_err());
+}
 
 #[test]
 fn forum_topic_icon_color() {
@@ -15,4 +40,15 @@ fn forum_topic_icon_color() {
         assert_eq!(ForumTopicIconColor::from(expected_value), expected_struct);
         assert_json_eq(expected_struct, serde_json::json!(expected_value));
     }
+}
+
+#[test]
+fn profile_accent_color() {
+    for i in 0..=15 {
+        let expected_struct = ProfileAccentColor::try_from(i).unwrap();
+        assert_eq!(Integer::from(expected_struct), i);
+        assert_json_eq(expected_struct, serde_json::json!(i));
+    }
+    assert!(ProfileAccentColor::try_from(-1).is_err());
+    assert!(ProfileAccentColor::try_from(16).is_err());
 }

a16c55c48e5f2ecc9643c8a04648cde22579e9ac
Ross Nomann <rossnomann@protonmail.com>
2025-02-12 15:32:32 +0100

    [Bot API 8.3] Update documentation for SetMessageReaction

--- src/types/reaction/mod.rs
+++ src/types/reaction/mod.rs
@@ -209,10 +209,10 @@ impl MessageReactionUpdated {
 
 /// Changes the chosen reactions on a message.
 ///
-/// Service messages can't be reacted to.
-/// Automatically forwarded messages from a channel to its discussion group have the same
-/// available reactions as messages in the channel.
-/// In albums, bots must react to the first message.
+/// Service messages of some types can't be reacted to.
+/// Automatically forwarded messages from a channel to its discussion group have
+/// the same available reactions as messages in the channel.
+/// Bots can't use paid reactions.
 #[derive(Clone, Debug, Serialize)]
 pub struct SetMessageReaction {
     chat_id: ChatId,

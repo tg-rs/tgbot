6cc94695fde03a41763ec838cab513dfee9afca8
Ross Nomann <rossnomann@protonmail.com>
2025-04-12 00:38:44 +0200

    [Bot API 9.0] Add InputProfilePhoto, InputProfilePhotoAnimated, InputProfilePhotoStatic types

--- src/types/input/mod.rs
+++ src/types/input/mod.rs
@@ -1,6 +1,7 @@
-pub use self::{file::*, media::*, message_content::*, paid_media::*};
+pub use self::{file::*, media::*, message_content::*, paid_media::*, profile_photo::*};
 
 mod file;
 mod media;
 mod message_content;
 mod paid_media;
+mod profile_photo;

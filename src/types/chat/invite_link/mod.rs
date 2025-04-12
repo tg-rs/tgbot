8916deb2b447ab2a22dd5ba0c41ec392768f2545
Ross Nomann <rossnomann@protonmail.com>
2025-04-12 09:38:14 +0200

    [Bot API 9.0] Update documentation

--- src/types/chat/invite_link/mod.rs
+++ src/types/chat/invite_link/mod.rs
@@ -250,7 +250,7 @@ impl CreateChatSubscriptionInviteLink {
     /// * `subscription_period` - The number of seconds the subscription will be active for before the next payment.
     ///   Currently, it must always be 2592000 (30 days).
     /// * `subscription_price` - The amount of Telegram Stars a user must pay initially
-    ///   and after each subsequent subscription period to be a member of the chat; 1-2500.
+    ///   and after each subsequent subscription period to be a member of the chat; 1-10000.
     pub fn new<T>(chat_id: T, subscription_period: Integer, subscription_price: Integer) -> Self
     where
         T: Into<ChatId>,

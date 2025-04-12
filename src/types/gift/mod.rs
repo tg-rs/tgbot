f7f01992af906fdf71ed59300c8ed0762c7d6827
Ross Nomann <rossnomann@protonmail.com>
2025-04-12 05:26:40 +0200

    [Bot API 9.0] Update gifts
    Added types:
    - OwnedGift
    - OwnedGiftRegular
    - OwnedGiftUnique
    - OwnedGifts
    - UniqueGift
    - UniqueGiftBackdrop
    - UniqueGiftBackdropColors
    - UniqueGiftModel
    - UniqueGiftSymbol

--- src/types/gift/mod.rs
+++ src/types/gift/mod.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::{
     api::{Method, Payload},
-    types::{ChatId, Integer, ParseMode, Sticker, TextEntities, TextEntity},
+    types::{ChatId, Integer, ParseMode, Sticker, TextEntities, TextEntity, User},
 };
 
 #[cfg(test)]
@@ -163,6 +163,400 @@ impl Method for GetAvailableGifts {
     fn into_payload(self) -> Payload {
         Payload::empty("getAvailableGifts")
     }
+}
+
+/// Describes a gift received and owned by a user or a chat.
+#[derive(Clone, Debug, derive_more::From, Deserialize, PartialEq, PartialOrd, Serialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+#[allow(clippy::large_enum_variant)]
+pub enum OwnedGift {
+    /// A regular gift owned by a user or a chat.
+    Regular(OwnedGiftRegular),
+    /// A unique gift received and owned by a user or a chat.
+    Unique(OwnedGiftUnique),
+}
+
+/// Describes a regular gift owned by a user or a chat.
+#[serde_with::skip_serializing_none]
+#[derive(Clone, Debug, Deserialize, PartialEq, PartialOrd, Serialize)]
+pub struct OwnedGiftRegular {
+    /// Information about the regular gift.
+    pub gift: Gift,
+    /// Date the gift was sent in Unix time.
+    pub send_date: Integer,
+    /// Whether the gift can be upgraded to a unique gift;
+    /// for gifts received on behalf of business accounts only.
+    pub can_be_upgraded: Option<bool>,
+    /// Number of Telegram Stars that can be claimed by the receiver instead of the gift;
+    /// omitted if the gift cannot be converted to Telegram Stars.
+    pub convert_star_count: Option<Integer>,
+    /// Special entities that appear in the text.
+    pub entities: Option<TextEntities>,
+    /// Whether the sender and gift text are shown only to the gift receiver;
+    /// otherwise, everyone will be able to see them.
+    pub is_private: Option<bool>,
+    /// Whether the gift is displayed on the account's profile page;
+    /// for gifts received on behalf of business accounts only.
+    pub is_saved: Option<bool>,
+    /// Unique identifier of the gift for the bot; for gifts received on behalf of business accounts only.
+    pub owned_gift_id: Option<String>,
+    /// Number of Telegram Stars that were paid by the sender for the ability to upgrade the gift.
+    pub prepaid_upgrade_star_count: Option<Integer>,
+    /// Sender of the gift if it is a known user.
+    pub sender_user: Option<User>,
+    /// Text of the message that was added to the gift.
+    pub text: Option<String>,
+    /// Whether the gift was refunded and isn't available anymore.
+    pub was_refunded: Option<bool>,
+}
+
+impl OwnedGiftRegular {
+    /// Creates a new `OwnedGiftRegular`.
+    ///
+    /// # Arguments
+    ///
+    /// * `gift` - Information about the regular gift.
+    /// * `send_date` - Date the gift was sent in Unix time.
+    pub fn new(gift: Gift, send_date: Integer) -> Self {
+        Self {
+            gift,
+            send_date,
+            can_be_upgraded: None,
+            convert_star_count: None,
+            entities: None,
+            is_private: None,
+            is_saved: None,
+            owned_gift_id: None,
+            prepaid_upgrade_star_count: None,
+            sender_user: None,
+            text: None,
+            was_refunded: None,
+        }
+    }
+
+    /// Sets a new value for the `can_be_upgraded` flag.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - Whether the gift can be upgraded to a unique gift;
+    ///   for gifts received on behalf of business accounts only.
+    pub fn with_can_be_upgraded(mut self, value: bool) -> Self {
+        self.can_be_upgraded = Some(value);
+        self
+    }
+
+    /// Sets a new convert star count.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - Number of Telegram Stars that can be claimed by the receiver instead of the gift;
+    ///   omitted if the gift cannot be converted to Telegram Stars.
+    pub fn with_convert_star_count(mut self, value: Integer) -> Self {
+        self.convert_star_count = Some(value);
+        self
+    }
+
+    /// Sets a new list of text entities.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - Special entities that appear in the text.
+    pub fn with_entities<T>(mut self, value: T) -> Self
+    where
+        T: IntoIterator<Item = TextEntity>,
+    {
+        self.entities = Some(value.into_iter().collect());
+        self
+    }
+
+    /// Sets a new value for the `is_private` flag.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - Whether the sender and gift text are shown only to the gift receiver;
+    ///   otherwise, everyone will be able to see them.
+    pub fn with_is_private(mut self, value: bool) -> Self {
+        self.is_private = Some(value);
+        self
+    }
+
+    /// Sets a new value for the `is_saved` flag.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - Whether the gift is displayed on the account's profile page;
+    ///   for gifts received on behalf of business accounts only.
+    pub fn with_is_saved(mut self, value: bool) -> Self {
+        self.is_saved = Some(value);
+        self
+    }
+
+    /// Sets a new owned gift ID.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - Unique identifier of the gift for the bot;
+    ///   for gifts received on behalf of business accounts only.
+    pub fn with_owned_gift_id<T>(mut self, value: T) -> Self
+    where
+        T: Into<String>,
+    {
+        self.owned_gift_id = Some(value.into());
+        self
+    }
+
+    /// Sets a new prepaid upgrade star count.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - Number of Telegram Stars that were paid by the sender for the ability to upgrade the gift.
+    pub fn with_prepaid_upgrade_star_count(mut self, value: Integer) -> Self {
+        self.prepaid_upgrade_star_count = Some(value);
+        self
+    }
+
+    /// Sets a new sender user.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - Sender of the gift if it is a known user.
+    pub fn with_sender_user(mut self, value: User) -> Self {
+        self.sender_user = Some(value);
+        self
+    }
+
+    /// Sets a new text.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - Text of the message that was added to the gift.
+    pub fn with_text<T>(mut self, value: T) -> Self
+    where
+        T: Into<String>,
+    {
+        self.text = Some(value.into());
+        self
+    }
+
+    /// Sets a new value for the `was_refunded` flag.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - Whether the gift was refunded and isn't available anymore.
+    pub fn with_was_refunded(mut self, value: bool) -> Self {
+        self.was_refunded = Some(value);
+        self
+    }
+}
+
+/// Describes a unique gift received and owned by a user or a chat.
+#[serde_with::skip_serializing_none]
+#[derive(Clone, Debug, Deserialize, PartialEq, PartialOrd, Serialize)]
+pub struct OwnedGiftUnique {
+    /// Information about the unique gift.
+    pub gift: UniqueGift,
+    /// Date the gift was sent in Unix time.
+    pub send_date: Integer,
+    /// Number of Telegram Stars that must be paid to transfer the gift;
+    /// omitted if the bot cannot transfer the gift.
+    pub transfer_star_count: Option<Integer>,
+    /// Whether the gift can be transferred to another owner;
+    /// for gifts received on behalf of business accounts only.
+    pub can_be_transferred: Option<bool>,
+    /// Whether the gift is displayed on the account's profile page;
+    /// for gifts received on behalf of business accounts only.
+    pub is_saved: Option<bool>,
+    /// Unique identifier of the received gift for the bot;
+    /// for gifts received on behalf of business accounts only.
+    pub owned_gift_id: Option<String>,
+    /// Sender of the gift if it is a known user.
+    pub sender_user: Option<User>,
+}
+
+impl OwnedGiftUnique {
+    /// Creates a new `OwnedGiftUnique`.
+    ///
+    /// # Arguments
+    ///
+    /// * `gift` - Information about the unique gift.
+    /// * `send_date` - Date the gift was sent in Unix time.
+    pub fn new(gift: UniqueGift, send_date: Integer) -> Self {
+        Self {
+            gift,
+            send_date,
+            transfer_star_count: None,
+            can_be_transferred: None,
+            is_saved: None,
+            owned_gift_id: None,
+            sender_user: None,
+        }
+    }
+
+    /// Sets a new transfer star count.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` Number of Telegram Stars that must be paid to transfer the gift;
+    ///   omitted if the bot cannot transfer the gift.
+    pub fn with_transfer_star_count(mut self, value: Integer) -> Self {
+        self.transfer_star_count = Some(value);
+        self
+    }
+
+    /// Sets a new value for the `can_be_transferred` flag.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - Whether the gift can be transferred to another owner;
+    ///   for gifts received on behalf of business accounts only.
+    pub fn with_can_be_transferred(mut self, value: bool) -> Self {
+        self.can_be_transferred = Some(value);
+        self
+    }
+
+    /// Sets a new value for the `is_saved` flag.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - Whether the gift is displayed on the account's profile page;
+    ///   for gifts received on behalf of business accounts only.
+    pub fn with_is_saved(mut self, value: bool) -> Self {
+        self.is_saved = Some(value);
+        self
+    }
+
+    /// Sets a new owned gift ID.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - Unique identifier of the received gift for the bot;
+    ///   for gifts received on behalf of business accounts only.
+    pub fn with_owned_gift_id<T>(mut self, value: T) -> Self
+    where
+        T: Into<String>,
+    {
+        self.owned_gift_id = Some(value.into());
+        self
+    }
+
+    /// Sets a new sender user.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - Sender of the gift if it is a known user.
+    pub fn with_sender_user(mut self, value: User) -> Self {
+        self.sender_user = Some(value);
+        self
+    }
+}
+
+/// Contains the list of gifts received and owned by a user or a chat.
+#[serde_with::skip_serializing_none]
+#[derive(Clone, Debug, Deserialize, PartialEq, PartialOrd, Serialize)]
+pub struct OwnedGifts {
+    /// The list of gifts.
+    pub gifts: Vec<OwnedGift>,
+    /// The total number of gifts owned by the user or the chat.
+    pub total_count: Integer,
+    /// Offset for the next request.
+    ///
+    /// If empty, then there are no more results.
+    pub next_offset: Option<String>,
+}
+
+impl OwnedGifts {
+    /// Creates a new `OwnedGifts`.
+    ///
+    /// # Arguments
+    ///
+    /// * `gifts` - The list of gifts.
+    /// * `total_count` - The total number of gifts owned by the user or the chat.
+    pub fn new<T>(gifts: T, total_count: Integer) -> Self
+    where
+        T: IntoIterator<Item = OwnedGift>,
+    {
+        Self {
+            gifts: gifts.into_iter().collect(),
+            total_count,
+            next_offset: None,
+        }
+    }
+
+    /// Sets a new next offset.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - Offset for the next request.
+    pub fn with_next_offset<T>(mut self, value: T) -> Self
+    where
+        T: Into<String>,
+    {
+        self.next_offset = Some(value.into());
+        self
+    }
+}
+
+/// Describes a unique gift that was upgraded from a regular gift.
+#[derive(Clone, Debug, Deserialize, PartialEq, PartialOrd, Serialize)]
+pub struct UniqueGift {
+    /// Backdrop of the gift.
+    pub backdrop: UniqueGiftBackdrop,
+    /// Human-readable name of the regular gift from which this unique gift was upgraded.
+    pub base_name: String,
+    /// Model of the gift.
+    pub model: UniqueGiftModel,
+    /// Unique name of the gift. This name can be used in https://t.me/nft/... links and story areas.
+    pub name: String,
+    /// Unique number of the upgraded gift among gifts upgraded from the same regular gift.
+    pub number: Integer,
+    ///  Symbol of the gift.
+    pub symbol: UniqueGiftSymbol,
+}
+
+/// Describes the backdrop of a unique gift.
+#[derive(Clone, Debug, Deserialize, PartialEq, PartialOrd, Serialize)]
+pub struct UniqueGiftBackdrop {
+    /// Colors of the backdrop.
+    pub colors: UniqueGiftBackdropColors,
+    /// Name of the backdrop.
+    pub name: String,
+    /// The number of unique gifts that receive this backdrop for every 1000 gifts upgraded.
+    pub rarity_per_mille: Integer,
+}
+
+/// Describes the colors of the backdrop of a unique gift.
+#[derive(Clone, Copy, Debug, Deserialize, PartialEq, PartialOrd, Serialize)]
+pub struct UniqueGiftBackdropColors {
+    /// The color in the center of the backdrop in RGB format
+    pub center_color: Integer,
+    /// The color on the edges of the backdrop in RGB format
+    pub edge_color: Integer,
+    /// The color to be applied to the symbol in RGB format
+    pub symbol_color: Integer,
+    /// The color for the text on the backdrop in RGB format
+    pub text_color: Integer,
+}
+
+/// Describes the model of a unique gift.
+#[derive(Clone, Debug, Deserialize, PartialEq, PartialOrd, Serialize)]
+pub struct UniqueGiftModel {
+    /// Name of the model.
+    pub name: String,
+    /// The number of unique gifts that receive this model for every 1000 gifts upgraded.
+    pub rarity_per_mille: Integer,
+    /// The sticker that represents the unique gift
+    pub sticker: Sticker,
+}
+
+/// Describes the symbol shown on the pattern of a unique gift.
+#[derive(Clone, Debug, Deserialize, PartialEq, PartialOrd, Serialize)]
+pub struct UniqueGiftSymbol {
+    /// Name of the symbol.
+    pub name: String,
+    /// The number of unique gifts that receive this model for every 1000 gifts upgraded.
+    pub rarity_per_mille: Integer,
+    /// The sticker that represents the unique gift.
+    pub sticker: Sticker,
 }
 
 /// Sends a gift to the given user.
--- src/types/gift/tests.rs
+++ src/types/gift/tests.rs
@@ -1,15 +1,25 @@
-use super::GetAvailableGifts;
 use crate::{
     api::{Payload, assert_payload_eq},
     types::{
         AcceptedGiftTypes,
+        GetAvailableGifts,
         Gift,
         Gifts,
+        OwnedGift,
+        OwnedGiftRegular,
+        OwnedGiftUnique,
+        OwnedGifts,
         ParseMode,
         SendGift,
         Sticker,
         StickerType,
         TextEntity,
+        UniqueGift,
+        UniqueGiftBackdrop,
+        UniqueGiftBackdropColors,
+        UniqueGiftModel,
+        UniqueGiftSymbol,
+        User,
         tests::assert_json_eq,
     },
 };
@@ -114,6 +124,250 @@ fn gifts() {
                     "star_count": 100,
                 }
             ]
+        }),
+    );
+}
+
+#[test]
+fn owned_gift() {
+    let expected_struct = OwnedGift::from(OwnedGiftRegular::new(
+        Gift::new(
+            "id",
+            Sticker::new("file-id", "file-unique-id", StickerType::Regular, 512, 512),
+            100,
+        ),
+        2,
+    ));
+    assert_json_eq(
+        expected_struct,
+        serde_json::json!({
+            "type": "regular",
+            "gift": {
+                "id": "id",
+                "sticker": {
+                    "file_id": "file-id",
+                    "file_unique_id": "file-unique-id",
+                    "type": "regular",
+                    "is_animated": false,
+                    "is_video": false,
+                    "height": 512,
+                    "width": 512,
+                },
+                "star_count": 100,
+            },
+            "send_date": 2,
+        }),
+    );
+    let expected_struct = OwnedGift::from(OwnedGiftUnique::new(
+        UniqueGift {
+            backdrop: UniqueGiftBackdrop {
+                colors: UniqueGiftBackdropColors {
+                    center_color: 1,
+                    edge_color: 2,
+                    symbol_color: 3,
+                    text_color: 4,
+                },
+                name: String::from("name"),
+                rarity_per_mille: 5,
+            },
+            base_name: String::from("base-name"),
+            model: UniqueGiftModel {
+                name: String::from("name"),
+                rarity_per_mille: 6,
+                sticker: Sticker::new("file-id", "file-unique-id", StickerType::Regular, 512, 512),
+            },
+            name: String::from("name"),
+            number: 7,
+            symbol: UniqueGiftSymbol {
+                name: String::from("name"),
+                rarity_per_mille: 8,
+                sticker: Sticker::new("file-id", "file-unique-id", StickerType::Regular, 512, 512),
+            },
+        },
+        9,
+    ));
+    assert_json_eq(
+        expected_struct,
+        serde_json::json!({
+            "type": "unique",
+            "gift": {
+                "backdrop": {
+                    "colors": {
+                        "center_color": 1,
+                        "edge_color": 2,
+                        "symbol_color": 3,
+                        "text_color": 4,
+                    },
+                    "name": "name",
+                    "rarity_per_mille": 5,
+                },
+                "base_name": "base-name",
+                "model": {
+                    "name": "name",
+                    "rarity_per_mille": 6,
+                    "sticker": {
+                        "file_id": "file-id",
+                        "file_unique_id": "file-unique-id",
+                        "type": "regular",
+                        "is_animated": false,
+                        "is_video": false,
+                        "height": 512,
+                        "width": 512,
+                    },
+                },
+                "name": "name",
+                "number": 7,
+                "symbol": {
+                    "name": "name",
+                    "rarity_per_mille": 8,
+                    "sticker": {
+                        "file_id": "file-id",
+                        "file_unique_id": "file-unique-id",
+                        "type": "regular",
+                        "is_animated": false,
+                        "is_video": false,
+                        "height": 512,
+                        "width": 512,
+                    },
+                },
+            },
+            "send_date": 9
+        }),
+    );
+}
+
+#[test]
+fn owned_gift_regular() {
+    let expected_struct = OwnedGiftRegular::new(
+        Gift::new(
+            "id",
+            Sticker::new("file-id", "file-unique-id", StickerType::Regular, 512, 512),
+            100,
+        ),
+        2,
+    );
+    assert_json_eq(
+        expected_struct.clone(),
+        serde_json::json!({
+            "gift": {
+                "id": "id",
+                "sticker": {
+                    "file_id": "file-id",
+                    "file_unique_id": "file-unique-id",
+                    "type": "regular",
+                    "is_animated": false,
+                    "is_video": false,
+                    "height": 512,
+                    "width": 512,
+                },
+                "star_count": 100,
+            },
+            "send_date": 2,
+        }),
+    );
+    assert_json_eq(
+        expected_struct
+            .with_can_be_upgraded(true)
+            .with_convert_star_count(3)
+            .with_entities([TextEntity::bold(0..2)])
+            .with_is_private(true)
+            .with_is_saved(true)
+            .with_owned_gift_id("id")
+            .with_prepaid_upgrade_star_count(4)
+            .with_sender_user(User::new(1, "John", false))
+            .with_text("test")
+            .with_was_refunded(false),
+        serde_json::json!({
+            "gift": {
+                "id": "id",
+                "sticker": {
+                    "file_id": "file-id",
+                    "file_unique_id": "file-unique-id",
+                    "type": "regular",
+                    "is_animated": false,
+                    "is_video": false,
+                    "height": 512,
+                    "width": 512,
+                },
+                "star_count": 100,
+            },
+            "send_date": 2,
+            "can_be_upgraded": true,
+            "convert_star_count": 3,
+            "entities": [{"type": "bold", "offset": 0, "length": 2}],
+            "is_private": true,
+            "is_saved": true,
+            "owned_gift_id": "id",
+            "prepaid_upgrade_star_count": 4,
+            "sender_user": {
+                "id": 1,
+                "first_name": "John",
+                "is_bot": false
+            },
+            "text": "test",
+            "was_refunded": false,
+        }),
+    );
+}
+
+#[test]
+fn owned_gifts() {
+    let expected_struct = OwnedGifts::new(
+        [OwnedGift::from(OwnedGiftRegular::new(
+            Gift::new(
+                "id",
+                Sticker::new("file-id", "file-unique-id", StickerType::Regular, 512, 512),
+                100,
+            ),
+            2,
+        ))],
+        1,
+    );
+    assert_json_eq(
+        expected_struct.clone(),
+        serde_json::json!({
+            "gifts": [{
+                "type": "regular",
+                "gift": {
+                    "id": "id",
+                    "sticker": {
+                        "file_id": "file-id",
+                        "file_unique_id": "file-unique-id",
+                        "type": "regular",
+                        "is_animated": false,
+                        "is_video": false,
+                        "height": 512,
+                        "width": 512,
+                    },
+                    "star_count": 100,
+                },
+                "send_date": 2,
+            }],
+            "total_count": 1,
+        }),
+    );
+    assert_json_eq(
+        expected_struct.with_next_offset("next"),
+        serde_json::json!({
+            "gifts": [{
+                "type": "regular",
+                "gift": {
+                    "id": "id",
+                    "sticker": {
+                        "file_id": "file-id",
+                        "file_unique_id": "file-unique-id",
+                        "type": "regular",
+                        "is_animated": false,
+                        "is_video": false,
+                        "height": 512,
+                        "width": 512,
+                    },
+                    "star_count": 100,
+                },
+                "send_date": 2,
+            }],
+            "total_count": 1,
+            "next_offset": "next",
         }),
     );
 }

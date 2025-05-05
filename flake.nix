3abb2f9be54584ad8b6cf04c3fb5c778996abd7c
Ross Nomann <rossnomann@protonmail.com>
2025-05-05 13:54:27 +0200

    nix flake update

--- flake.nix
+++ flake.nix
@@ -1,7 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs =

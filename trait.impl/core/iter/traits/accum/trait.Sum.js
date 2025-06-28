1bf7450acd24a4438d4b8ff85c5d8ea316f34235
rossnomann <rossnomann@users.noreply.github.com>
2025-06-28 18:04:25 +0000

    deploy: 989cd4f1d419caff36ebcecbce43a96f0b04f020

---
+++ trait.impl/core/iter/traits/accum/trait.Sum.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["writeable",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/accum/trait.Sum.html\" title=\"trait core::iter::traits::accum::Sum\">Sum</a> for <a class=\"struct\" href=\"writeable/struct.LengthHint.html\" title=\"struct writeable::LengthHint\">LengthHint</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/accum/trait.Sum.html\" title=\"trait core::iter::traits::accum::Sum\">Sum</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"writeable/struct.LengthHint.html\" title=\"struct writeable::LengthHint\">LengthHint</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[691]}

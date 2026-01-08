c3f655d4a5be06d76402cfac27ebd7e3557bc95c
Ross Nomann <rossnomann@protonmail.com>
2026-01-08 17:42:29 +0100

    nixos-unstable > nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";

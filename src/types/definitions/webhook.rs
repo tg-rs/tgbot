78a546728c593c95564bc60e2571ab681e91b731
Ross Nomann <rossnomann@protonmail.com>
2026-08-02 21:00:22 +0200

    Update webhook
    - Remove unused constructor and setters from WebhookInfo
    - Use InputFileReader for the certificate parameter in SetWebhook

--- src/types/definitions/webhook.rs
+++ src/types/definitions/webhook.rs
@@ -1,10 +1,8 @@
-use std::collections::HashSet;
-
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    api::{Method, Payload, PayloadError},
-    types::{AllowedUpdate, Integer},
+    api::{Form, Method, Payload, PayloadError, WriteForm},
+    types::{AllowedUpdate, InputFileReader, Integer},
 };
 
 /// Represents a current status of a webhook.
@@ -35,101 +33,6 @@ pub struct WebhookInfo {
     /// Maximum allowed number of simultaneous HTTPS connections
     /// to the webhook for update delivery.
     pub max_connections: Option<Integer>,
-}
-
-impl WebhookInfo {
-    /// Creates a new `WebhookInfo`.
-    ///
-    /// # Arguments
-    ///
-    /// * `url` - Webhook URL.
-    /// * `has_custom_certificate` - Indicates whether a custom certificate was provided.
-    /// * `pending_update_count` - Number of updates awaiting delivery.
-    pub fn new<T>(url: T, has_custom_certificate: bool, pending_update_count: Integer) -> Self
-    where
-        T: Into<String>,
-    {
-        Self {
-            has_custom_certificate,
-            pending_update_count,
-            url: url.into(),
-            allowed_updates: None,
-            ip_address: None,
-            last_error_date: None,
-            last_error_message: None,
-            last_synchronization_error_date: None,
-            max_connections: None,
-        }
-    }
-
-    /// Sets a new list of allowed updates.
-    ///
-    /// # Arguments
-    ///
-    /// * `value` - List of allowed updates.
-    pub fn with_allowed_updates<T>(mut self, value: T) -> Self
-    where
-        T: IntoIterator<Item = AllowedUpdate>,
-    {
-        self.allowed_updates = Some(value.into_iter().collect());
-        self
-    }
-
-    /// Sets a new IP address.
-    ///
-    /// # Arguments
-    ///
-    /// * `value` - IP address.
-    pub fn with_ip_address<T>(mut self, value: T) -> Self
-    where
-        T: Into<String>,
-    {
-        self.ip_address = Some(value.into());
-        self
-    }
-
-    /// Sets a new last error date.
-    ///
-    /// # Arguments
-    ///
-    /// * `value` - Last error date.
-    pub fn with_last_error_date(mut self, value: Integer) -> Self {
-        self.last_error_date = Some(value);
-        self
-    }
-
-    /// Sets a new last error message.
-    ///
-    /// # Arguments
-    ///
-    /// * `value` - Last error message.
-    pub fn with_last_error_message<T>(mut self, value: T) -> Self
-    where
-        T: Into<String>,
-    {
-        self.last_error_message = Some(value.into());
-        self
-    }
-
-    /// Sets a new last synchronization error date.
-    ///
-    /// # Arguments
-    ///
-    /// * `value` - Last synchronization error date.
-    pub fn with_last_synchronization_error_date(mut self, value: Integer) -> Self {
-        self.last_synchronization_error_date = Some(value);
-        self
-    }
-
-    /// Sets a new number of max connections.
-    ///
-    /// # Arguments
-    ///
-    /// * `value` - Number of max connections.
-    pub fn with_max_connections(mut self, value: Integer) -> Self {
-        self.max_connections = Some(value);
-        self
-    }
 }
 
 /// Removes a webhook integration if you decide to switch back to [`crate::types::GetUpdates`].
@@ -183,16 +86,10 @@ impl Method for GetWebhookInfo {
 /// If you'd like to make sure that the Webhook request comes from Telegram,
 /// we recommend using a secret path in the URL, e.g. `https://www.example.com/<token>`
 /// Since nobody else knows your bot's token, you can be pretty sure it's us.
-#[serde_with::skip_serializing_none]
-#[derive(Clone, Debug, Serialize)]
+#[derive(Debug)]
 pub struct SetWebhook {
-    url: String,
-    allowed_updates: Option<HashSet<AllowedUpdate>>,
-    certificate: Option<String>,
-    drop_pending_updates: Option<bool>,
-    ip_address: Option<String>,
-    max_connections: Option<Integer>,
-    secret_token: Option<String>,
+    certificate: Option<InputFileReader>,
+    parameters: SetWebhookParams,
 }
 
 impl SetWebhook {
@@ -206,33 +103,12 @@ impl SetWebhook {
         T: Into<String>,
     {
         Self {
-            url: url.into(),
-            allowed_updates: None,
             certificate: None,
-            drop_pending_updates: None,
-            ip_address: None,
-            max_connections: None,
-            secret_token: None,
+            parameters: SetWebhookParams {
+                url: Some(url.into()),
+                ..Default::default()
+            },
         }
-    }
-
-    /// Adds a type of update you want your bot to receive.
-    ///
-    /// # Arguments
-    ///
-    /// * `value` - A type to add.
-    pub fn add_allowed_update(mut self, value: AllowedUpdate) -> Self {
-        match self.allowed_updates {
-            Some(ref mut updates) => {
-                updates.insert(value);
-            }
-            None => {
-                let mut updates = HashSet::new();
-                updates.insert(value);
-                self.allowed_updates = Some(updates);
-            }
-        };
-        self
     }
 
     /// Sets a new list of allowed update types.
@@ -243,14 +119,21 @@ impl SetWebhook {
     ///
     /// For example, specify `[AllowedUpdate::Message]`
     /// to only receive updates of these types.
+    ///
     /// See [`AllowedUpdate`] for a complete list of available update types.
+    ///
     /// Specify an empty list to receive all updates regardless of type (default).
+    ///
     /// If not specified, the previous setting will be used.
+    ///
     /// Please note that this parameter doesn't affect
     /// updates created before the call to the [`SetWebhook`],
     /// so unwanted updates may be received for a short period of time.
-    pub fn with_allowed_updates(mut self, value: HashSet<AllowedUpdate>) -> Self {
-        self.allowed_updates = Some(value);
+    pub fn with_allowed_updates<T>(mut self, value: T) -> Self
+    where
+        T: IntoIterator<Item = AllowedUpdate>,
+    {
+        self.parameters.allowed_updates = Some(value.into_iter().collect());
         self
     }
 
@@ -261,7 +144,7 @@ impl SetWebhook {
     /// * `value` - Public key certificate; so that the root certificate in use can be checked.
     pub fn with_certificate<T>(mut self, value: T) -> Self
     where
-        T: Into<String>,
+        T: Into<InputFileReader>,
     {
         self.certificate = Some(value.into());
         self
@@ -273,7 +156,7 @@ impl SetWebhook {
     ///
     /// * `value` - Indicates whether to drop all pending updates.
     pub fn with_drop_pending_updates(mut self, value: bool) -> Self {
-        self.drop_pending_updates = Some(value);
+        self.parameters.drop_pending_updates = Some(value);
         self
     }
 
@@ -287,7 +170,7 @@ impl SetWebhook {
     where
         T: Into<String>,
     {
-        self.ip_address = Some(value.into());
+        self.parameters.ip_address = Some(value.into());
         self
     }
 
@@ -301,7 +184,7 @@ impl SetWebhook {
     /// Use lower values to limit the load on your bot's server,
     /// and higher values to increase your bot’s throughput.
     pub fn with_max_connections(mut self, value: Integer) -> Self {
-        self.max_connections = Some(value);
+        self.parameters.max_connections = Some(value);
         self
     }
 
@@ -319,15 +202,34 @@ impl SetWebhook {
     where
         T: Into<String>,
     {
-        self.secret_token = Some(value.into());
+        self.parameters.secret_token = Some(value.into());
         self
     }
+}
+
+#[serde_with::skip_serializing_none]
+#[derive(Debug, Default, Serialize)]
+struct SetWebhookParams {
+    url: Option<String>,
+    allowed_updates: Option<Vec<AllowedUpdate>>,
+    certificate: Option<String>,
+    drop_pending_updates: Option<bool>,
+    ip_address: Option<String>,
+    max_connections: Option<Integer>,
+    secret_token: Option<String>,
 }
 
 impl Method for SetWebhook {
     type Response = bool;
 
     fn into_payload(self) -> Result<Payload, PayloadError> {
-        Payload::json("setWebhook", self)
+        let Self {
+            certificate,
+            mut parameters,
+        } = self;
+        let mut form = Form::default();
+        parameters.certificate = certificate.map(|x| x.write(&mut form));
+        parameters.serialize(&mut form)?;
+        Payload::form("setWebhook", form)
     }
 }

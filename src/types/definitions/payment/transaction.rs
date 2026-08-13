72fd5f2607a2561f8cfece6b76734bb9e3b7ba5e
Ross Nomann <rossnomann@protonmail.com>
2026-08-13 21:45:30 +0200

    Fix inaccessible StarTransaction fields

--- src/types/definitions/payment/transaction.rs
+++ src/types/definitions/payment/transaction.rs
@@ -27,12 +27,26 @@ where
 #[serde_with::skip_serializing_none]
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 pub struct StarTransaction {
-    amount: Integer,
-    date: Integer,
-    id: String,
-    nanostar_amount: Option<Integer>,
-    source: Option<TransactionPartner>,
-    receiver: Option<TransactionPartner>,
+    /// Integer amount of Telegram Stars transferred by the transaction.
+    pub amount: Integer,
+    /// Date the transaction was created in Unix time.
+    pub date: Integer,
+    /// Unique identifier of the transaction.
+    ///
+    /// Coincides with the identifier of the original transaction
+    /// for refund transactions.
+    ///
+    /// Coincides with SuccessfulPayment.telegram_payment_charge_id
+    /// for successful incoming payments from users.
+    pub id: String,
+    /// The number of 1/1000000000 shares of
+    /// Telegram Stars transferred by the transaction;
+    /// from 0 to 999999999.
+    pub nanostar_amount: Option<Integer>,
+    /// Source of an incoming transaction.
+    pub source: Option<TransactionPartner>,
+    /// Receiver of an outgoing transaction.
+    pub receiver: Option<TransactionPartner>,
 }
 
 impl StarTransaction {
